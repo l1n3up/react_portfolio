10347044d27ec732c46c48cf699785fd00e7c1ae
l1n3up <ironhide610@gmail.com>
2024-05-28 02:22:46 +0200

    fixed double names in contact->textarea

--- components/contact.tsx
+++ components/contact.tsx
@@ -31,7 +31,7 @@ export default function Contact() {
       </p>
       <form
         action={async formData => {
-          const { data, error } = await sendEmail(formData);
+          const { error } = await sendEmail(formData);
 
           if (error) {
             toast.error(error);
